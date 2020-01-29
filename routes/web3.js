a424a02920c16962a1b99a5d3cd820346eac6d3a
seonghunYang <didtjdgns852@naver.com>
2020-01-29 19:52:28 +0900

    web3 test

---
+++ routes/web3.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const catchErrors = require('../lib/async-error');
+const Product = require('../models/product');
+const Category = require('../models/category');
+
+/* GET users listing. */
+
+module.exports = router;
