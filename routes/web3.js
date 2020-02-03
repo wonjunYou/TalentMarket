b960f2f4b41c32fd286c73a013feaf2c9d2844d1
seonghunYang <didtjdgns852@naver.com>
2020-02-03 16:44:08 +0900

    backend web3

--- routes/web3.js
+++ routes/web3.js
@@ -3,7 +3,9 @@ var router = express.Router();
 const catchErrors = require('../lib/async-error');
 const Product = require('../models/product');
 const Category = require('../models/category');
+const Web3 = require('web3');
 
+let web3 = new Web3(new web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/1aaf0adf6b8c495190259f11e9f997ea"))
 /* GET users listing. */
 
 module.exports = router;
